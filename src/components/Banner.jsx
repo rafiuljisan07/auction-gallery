803749df3138a47ccc38cbb6dc712c12c4c8fcce
Rafiul Islam Jisan <rafiuljisan2005@gmail.com>
2025-09-27 23:05:11 +0600

    initialized the project, added navbar and banner

---
+++ src/components/Banner.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import BannerImg from '../assets/Banner-min.jpg'
+import Container from './Container';
+
+const Banner = () => {
+    return (
+        <div className='h-[550px] bg-right bg-cover' style={{ backgroundImage: `url(${BannerImg})` }}>
+            <Container>
+                <div className='text-white flex flex-col justify-center h-[550px] '>
+                    <h1 className='text-6xl font-semibold'>Bid on Unique Items from <br />Around the World</h1>
+                    <p className='text-xl my-6 text-gray-300'>Discover rare collectibles, luxury goods, and vintage <br />treasures in our curated auctions</p>
+                    <button className=' btn w-fit  text-black text-xl p-6 rounded-4xl'>Explore Auctions</button>
+                </div>
+            </Container>
+        </div>
+    );
+};
+
+export default Banner;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import Container from './Container';
+
+const Navbar = () => {
+    return (
+        <div className='bg-base-100 shadow-sm'>
+            <Container>
+                <div className="navbar">
+                    <div className="navbar-start">
+                        <a className="btn btn-ghost text-4xl text-blue-800 font-normal">Auction <span className='font-extrabold text-4xl text-yellow-500'>Gallery</span></a>
+                    </div>
+                    <div className='navbar-center gap-9 text-2xl '>
+                        <a href="">Home</a>
+                        <a href="">Actions</a>
+                        <a href="">Categories</a>
+                        <a href="">How to Work</a>
+                    </div>
+                    <div className="navbar-end">
+                        <div tabIndex={0} role="button" className="btn btn-ghost btn-circle">
+                            <div className="indicator mr-6b">
+                                <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" /> </svg>
+                                <span className="badge badge-sm indicator-item">8</span>
+                            </div>
+                        </div>
+                        <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
+                            <div className="w-10 rounded-full">
+                                <img
+                                    alt="Tailwind CSS Navbar component"
+                                    src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </Container>
+        </div>
+    );
+};
+
+export default Navbar;
